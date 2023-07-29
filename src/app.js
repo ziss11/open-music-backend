6026932c6d41558eadbe5e9bc2a6300aeddd2c58
Abdul Azis <abdul.azizz1108@gmail.com>
2023-07-29 12:19:57 +0700

    error handling implemented

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const songs = require('./api/songs')
 const AlbumsService = require('./services/postgres/AlbumsService')
 const SongsService = require('./services/postgres/SongsService')
 const Validator = require('./validator')
+const ClientError = require('./exceptions/ClientError')
 
 require('dotenv').config()
 
@@ -19,6 +20,36 @@ const init = async () => {
         origin: ['*']
       }
     }
+  })
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request
+
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message
+        })
+
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+
+      if (!response.isServer) {
+        return h.continue
+      }
+
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kesalahan pada server kami'
+      })
+
+      newResponse.code(500)
+      return newResponse
+    }
+
+    return h.continue
   })
 
   await server.register([
